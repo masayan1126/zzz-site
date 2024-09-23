b78b6be55134cd782c97363aaf012f203ca8ce29
masayan1126 <masa199311266@gmail.com>
2024-09-23 10:24:30 +0900

    テスト修正

--- shared/components/PageTitle.test.tsx
+++ shared/components/PageTitle.test.tsx
@@ -1,7 +1,7 @@
-// import { render, screen } from "@testing-library/react";
-// import "@testing-library/jest-dom";
-// import { expect, jest, test, describe } from "@jest/globals";
-// import PageTitle from "@/shared/components/PageTitle";
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import { expect, jest, test, describe } from "@jest/globals";
+import PageTitle from "@/shared/components/PageTitle";
 // test("loads and displays greeting", async () => {
 //   // ARRANGE
 //   render(<PageTitle title="aa" />);
@@ -9,3 +9,7 @@
 //   // ASSERT
 //   expect(screen.getByRole("heading")).toHaveTextContent("aa");
 // });
+
+test("check", () => {
+  expect(1).toBe(1);
+});
